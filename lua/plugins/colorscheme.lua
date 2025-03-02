f333dcffe39ee8ad690e3cc4c88d1ee6ccbd492f
braam76 <ilgargamidov968@gmail.com>
2025-03-02 12:55:03 +0300

    formatted lua files

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    "Shatur/neovim-ayu",
-    lazy = false,
-    priority = 1000,
-    mirage = false, 
-    terminal = true,
-    overrides = {},
- 
-    config = function()
-        vim.cmd[[colorscheme ayu-mirage]]
-    end,
+	"Shatur/neovim-ayu",
+	lazy = false,
+	priority = 1000,
+	mirage = false,
+	terminal = true,
+	overrides = {},
+
+	config = function()
+		vim.cmd([[colorscheme ayu-mirage]])
+	end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,16 +1,15 @@
-
 return {
-  "stevearc/oil.nvim",
-  cmd = { "Oil" }, -- This ensures it loads when the command is used
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional dependency
-  keys = {
-    { "<leader>-", "<cmd>Oil<CR>", desc = "File Explorer" },
-  },
-  lazy = false,
-  opts = {
-    default_file_explorer = true, -- Replaces netrw
-    view_options = {
-      show_hidden = true, -- Show hidden files
-    },
-  },
+	"stevearc/oil.nvim",
+	cmd = { "Oil" }, -- This ensures it loads when the command is used
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional dependency
+	keys = {
+		{ "<leader>-", "<cmd>Oil<CR>", desc = "File Explorer" },
+	},
+	lazy = false,
+	opts = {
+		default_file_explorer = true, -- Replaces netrw
+		view_options = {
+			show_hidden = true, -- Show hidden files
+		},
+	},
 }
